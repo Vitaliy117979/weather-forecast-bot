eba4dbb1d837ab958a8d8186c27acdcaaa50d3b5
Vitaliy117979 <privedenyuk2006@gmail.com>
2023-12-12 19:27:37 +0200

    update polling_error

--- app.mjs
+++ app.mjs
@@ -13,9 +13,9 @@ const { TG_BOT_ID } = process.env;
 export const bot = new TelegramBot(TG_BOT_ID, {
   polling: true,
 });
-
-bot.on("polling_error", err => console.log("error"));
-
+bot.on("polling_error", err => {
+  console.log("Error occurred:", err);
+});
 export const initializeBot = () => {
   bot.on("text", async (msg) => {
     const text = msg.text;
